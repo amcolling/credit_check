2884d42f958ef5a13bd4b54fb6ad53f619b92479
amcolling <amcolling@gmail.com>
2018-03-18 13:09:11 -0600

    credit_check

---
+++ credit_check_test.rb
@@ -0,0 +1,31 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/credit_check'
+
+  class CreditCheckTest < Minitest::Test
+    def test_credit_check_test_exists
+    credit_check = CreditCheck.new("4929735477250543")
+    assert_instance_of CreditCheck, credit_check
+  end
+
+  def test_credit_card_number
+    credit_check = CreditCheck.new("4929735477250543")
+    assert_equal "4929735477250543", credit_check.credit_card_number
+  end
+
+  def test_convert_to_array
+    credit_check = CreditCheck.new ("4929735477250543")
+    assert_equal [3, 4, 5, 0, 5, 2, 7, 7, 4, 5, 3, 7, 9, 2, 9, 4], credit_check.convert_to_array
+  end
+
+  def test_every_other_digit
+    credit_check = CreditCheck.new ("4929735477250543")
+    assert_equal [3, 8, 5, 0, 5, 4, 7,14, 4, 10, 3, 14, 9, 4, 9, 8], credit_check.every_other_digit
+  end
+
+  def sum_of_it_all
+    credit_check = CreditCheck.new ("4929735477250543")
+    assert_equal "This number is valid!", num,sum_it_all
+  end
+end
---
+++ lib/credit_check.rb
@@ -0,0 +1,65 @@
+class CreditCheck
+attr_reader :credit_card_number,
+            :convert_to_array,
+            :every_other_digit,
+            :sum_of_it_all
+
+
+  def initialize(credit_card_number)
+    @credit_card_number = credit_card_number
+    end
+
+  def convert_to_array
+    card_number_array = @credit_card_number.to_i.digits
+
+    end
+
+  def every_other_digit
+    doubled = convert_to_array.map.with_index do |digit, index|
+      if index.odd? == true
+        digit * 2
+      else
+      digit
+    end
+  end
+end
+
+
+
+  def sum_of_it_all
+    over_ten = every_other_digit.map do|num|
+    if num.digits.sum % 10 == 0
+    puts "This number is valid!"
+  else
+    puts "This number is not valid!"
+    end
+  end
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Your Luhn Algorithm Here
+
+
+
+
+# Output
+## If it is valid, print "The number is valid!"
+## If it is invalid, print "The number is invalid!"
